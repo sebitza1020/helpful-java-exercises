888cceb73a5456353c078546c626526b42a647a3
sebitza1020 <munteansebastian@gmail.com>
2021-07-16 13:06:02 +0300

    Changes made at collections package from challenges.

--- src/com/sebitza/javaexercises/Challenges/collections/map/Main.java
+++ src/com/sebitza/javaexercises/Challenges/collections/map/Main.java
@@ -1,42 +1,35 @@
-package com.sebitza.javaexercises.Challenges.immutable;
+package com.sebitza.javaexercises.Challenges.collections.map;
 
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
-    private Map<Integer, Location> locations = new HashMap<>();
-    private Map<String, String> vocabulary = new HashMap<>();
-    private Map<String, Integer> tempExit;
+    private Map<Integer, Location> locations = new HashMap<Integer, Location>();
+    private Map<String, String> vocabulary = new HashMap<String, String>();
 
     public Main() {
-        locations.put(0, new Location(0, "You are sitting in front of a computer learning Java", null));
+        locations.put(0, new Location(0, "You are sitting in front of a computer learning Java"));
+        locations.put(1, new Location(1, "You are standing at the end of a road before a small brick building"));
+        locations.put(2, new Location(2, "You are at the top of a hill"));
+        locations.put(3, new Location(3, "You are inside a building, a well house for a small spring"));
+        locations.put(4, new Location(4, "You are in a valley beside a stream"));
+        locations.put(5, new Location(5, "You are in the forest"));
 
-        tempExit = new HashMap<>();
-        tempExit.put("W", 2);
-        tempExit.put("E", 3);
-        tempExit.put("S", 4);
-        tempExit.put("N", 5);
-        locations.put(1, new Location(1, "You are standing at the end of a road before a small brick building", tempExit));
+        locations.get(1).addExit("W", 2);
+        locations.get(1).addExit("E", 3);
+        locations.get(1).addExit("S", 4);
+        locations.get(1).addExit("N", 5);
 
-        tempExit = new HashMap<>();
-        tempExit.put("N", 5);
-        locations.put(2, new Location(2, "You are at the top of a hill", tempExit));
+        locations.get(2).addExit("N", 5);
 
-        tempExit = new HashMap<>();
-        tempExit.put("W", 1);
-        locations.put(3, new Location(3, "You are inside a building, a well house for a small spring", tempExit));
+        locations.get(3).addExit("W", 1);
 
-        tempExit = new HashMap<>();
-        tempExit.put("N", 1);
-        tempExit.put("W", 2);
-        locations.put(4, new Location(4, "You are in a valley beside a stream", tempExit));
+        locations.get(4).addExit("N", 1);
+        locations.get(4).addExit("W", 2);
 
-        tempExit = new HashMap<>();
-        tempExit.put("S", 1);
-        tempExit.put("W", 2);
-        locations.put(5, new Location(5, "You are in the forest", tempExit));
-
+        locations.get(5).addExit("S", 1);
+        locations.get(5).addExit("W", 2);
     }
 
     public void command() {
@@ -50,8 +43,6 @@ public class Main {
         int loc = 1;
         while (true) {
             System.out.println(locations.get(loc).getDescription());
-            tempExit.remove("S");
-
             if (loc == 0) {
                 break;
             }
