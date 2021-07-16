dd84fdc0559456efaef941782b91dce390b5dea5
sebitza1020 <munteansebastian@gmail.com>
2021-07-16 13:03:21 +0300

    1 challenge and 1 exercise from sets have been added.

---
+++ src/com/sebitza/javaexercises/Challenges/collections/sets/DwarfPlanet.java
@@ -0,0 +1,7 @@
+package com.sebitza.javaexercises.Challenges.collections.sets;
+
+public class DwarfPlanet extends HeavenlyBody {
+    public DwarfPlanet(String name, double orbitalPeriod) {
+        super(name, orbitalPeriod, BodyTypes.DWARF_PLANET);
+    }
+}
---
+++ src/com/sebitza/javaexercises/Challenges/collections/sets/SetChallenge.java
@@ -0,0 +1,7 @@
+package com.sebitza.javaexercises.Challenges.collections.sets;
+
+public class SetChallenge {
+    public static void main(String[] args) {
+
+    }
+}
